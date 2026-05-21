1026a9b7919a69eddb10dc42daba9aac7cce2356
KB Developers <tarunsai3012@gmail.com>
2026-05-21 18:00:37 +0530

    feat: add get_current_user to verify JWT on protected routes

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,12 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from database import AsyncSessionLocal
+from fastapi import Depends, HTTPException, status, Request
+from models import UserModel
 import os
 from dotenv import load_dotenv
 
@@ -18,6 +24,37 @@ def create_token(data: dict):                                                   
     to_encode.update({"exp": expire})                                            #include calculated expiry time
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)   #scrimbles the data into token
     return encoded_jwt
+
+async def gets_current_user(
+    token:str=Depends(token_extractor),
+):
+    credentials_error=HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,    #lets create error block related to invalid tokens so i can use this block whereever i need to display the error
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try: 
+        payload=jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email:str=payload.get("sub")
+    
+        if email is None:
+            raise credentials_error    #display error message if email not found
+            
+
+    except JWTError:
+        raise credentials_error         #display error message if token expired or invalid or tampered token
+
+    async with AsyncSessionLocal() as db:
+        result=await db.execute(
+            select(UserModel).where(UserModel.email == email) #sql
+        )
+        user=result.scalar_one_or_none()
+
+
+    if user is None:                      #display error message if email not found in database
+        raise credentials_error
+    return user
 
 
 def refresh_token(data: dict):                                                   # dict is the type of data
